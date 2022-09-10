112d1cea576839b99736f99470c4b00497f558b7
Leon-Parepko <l.parepko@innopolis.university>
2022-09-10 07:46:22 +0300

    Finished data augmentation and combination algorithm.

--- augmentations.py
+++ augmentations.py
@@ -1,25 +1,23 @@
-# import torch.nn as nn
+import numpy as np
 import torchvision.transforms as tr
 
-transforms = tr.Compose([
+final_transform = tr.Compose([
     tr.ToPILImage(),
-    # tr.RandomCrop((47, 27)),
-    tr.ColorJitter(brightness=1),
-    tr.RandomRotation(degrees=30),
-    tr.RandomHorizontalFlip(),
-    tr.RandomVerticalFlip(),
+    # tr.ColorJitter(brightness=1),
+    tr.RandomRotation(degrees=15),
     tr.RandomPerspective(),
     # tr.RandomAffine(degrees=30),
-    tr.RandomAdjustSharpness(sharpness_factor=3),
-    tr.GaussianBlur(kernel_size=9),
+    # tr.RandomAdjustSharpness(sharpness_factor=3),
+    # tr.GaussianBlur(kernel_size=9),
 ])
 
 
 symbol_transform = tr.Compose([
     tr.ToPILImage(),
-    tr.Resize(37),
+    tr.Resize(42),
     tr.RandomCrop((47, 27)),
-    tr.GaussianBlur(kernel_size=21),
+    tr.RandomAdjustSharpness(sharpness_factor=np.random.uniform(low=0, high=8), p=0.8),
+    # tr.GaussianBlur(kernel_size=17),
     tr.ToTensor()
     # tr.ColorJitter(brightness=1),
 
@@ -29,6 +27,12 @@ symbol_transform = tr.Compose([
     # tr.RandomPerspective(),
     # # tr.RandomAffine(degrees=30),
     # tr.RandomAdjustSharpness(sharpness_factor=3),
+])
 
 
+background_transform = tr.Compose([
+    tr.ToPILImage(),
+    tr.GaussianBlur(kernel_size=27),
+    # tr.ColorJitter(brightness=1),
+    tr.ToTensor()
 ])
