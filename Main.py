916d50cca7e1cb456b861198279cde5371e8b209
Leon-Parepko <l.parepko@innopolis.university>
2022-09-15 03:48:55 +0300

    Finaly got first results! Basic CNN could already print arts. Add new CNN with skip connections.

--- Main.py
+++ Main.py
@@ -6,32 +6,35 @@
 """
 
 # Import all libraries
+import random
 import numpy as np
 import torchvision
 from tqdm import tqdm
 import torch
 import Functional
 from data_loaders import ImgLoader
-from Models import cnn_model
+import Models
 from datasets import AugmentedDataset
 import matplotlib.pyplot as plt
 import time
 
 # Load image
-img = Functional.load_image("Data/Dataset/Font.png")
+img = Functional.load_image("Data/Dataset/Test.jpg")
+img = torchvision.transforms.Grayscale()(img)
 depth, high, width = img.shape
 img = img.reshape(high, width)
 
 # Configure model parameters
-# used_symbols = [...]
+used_symbols = "<)«¬-¯/+-(»‹›_¤+>EFHKLOT}{|~^]_\["
+magic_number = 69
 mode = "train"
-model_data = torch.load("Data/Model_2e.pth")
+model_data = torch.load("Data/Model_Configs/Basic_CNN_2e.pth")
 
 
 
 
 device = Functional.gpu_check()
-model = cnn_model(33).to(device)
+model = Models.cnn_model_v2(33).to(device)
 
 if mode == "train":
 
@@ -45,14 +48,16 @@ if mode == "train":
     batch_loader = ImgLoader(dataset=dataset, batch_size=50, device=device)
     train_batch_loader, valid_batch_loader = batch_loader.get_loaders(split_ratio=0.9)
 
-    model.train(mode=True)
+    model.train()
     epochs = 3
     loss_func = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
     history = []
 
     for epoch in range(0, epochs):
-        np.random.seed(44)
+        torch.manual_seed(magic_number)
+        np.random.seed(magic_number)
+        random.seed(magic_number)
 
         for i, (img, ans) in enumerate(tqdm(train_batch_loader)):  # 0.30s
             ans = torch.nn.functional.one_hot(ans, num_classes=33)
@@ -78,17 +83,31 @@ if mode == "train":
                       "epoch": epoch,
                       "history": history}
 
-        torch.save(parameters, f"Data/Model_{epoch}e.pth")
+        torch.save(parameters, f"Data/Model_Configs/{model.name}_{epoch}e.pth")
 
 if mode == "test":
-    plt.imshow(img.reshape(320, 609), cmap='Greys')
+    # print(summary(model, (1, 47, 27), batch_size=50))
+    plt.imshow(img, cmap='Greys')
     plt.show()
 
     model.load_state_dict(model_data["model"])
     model.eval()
 
     with torch.no_grad():
-        pass
+        out_art = ""
+
+        for i in range(0, high // 47):
+            out_art += "\n"
+            for j in range(0, width // 27):
+                img_segment = img[i*47:(i+1)*47, j*27:(j+1)*27]
+                img_segment = img_segment.reshape(1, 47, 27).to(device)
+                pred = model.forward(img_segment)
+                pred = torch.nn.Sigmoid()(pred).argmax()
+                out_art += used_symbols[pred]
+
+        print(out_art)
+                
+            
 
 # start_time = time.time()
 # print(time.time() - start_time)
--- Models.py
+++ Models.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class cnn_model(nn.Module):
     def __init__(self, out_class_num):
         super(cnn_model, self).__init__()
+        self.name = "Basic_CNN"
         self.conv_model = nn.Sequential(
             nn.Conv2d(1, 10, 3, stride=1),
             nn.ReLU(),
@@ -45,48 +46,77 @@ class cnn_model(nn.Module):
 
 
 
+# This one is quite similar to ResNet
+class cnn_model_v2(nn.Module):
+
+    def __init__(self, out_class_num):
+        super(cnn_model_v2, self).__init__()
+        self.name = "ResCNN_v1"
+        self.block_1 = nn.Sequential(
+            self.conv_builder(1, 10, 1),
+            self.conv_builder(10, 30, 3, padding=1),
+            self.conv_builder(30, 50, 3, padding=1),
+            nn.MaxPool2d(2)
+        )
+
+        self.downsample_1 = self.downsample(1, 50, maxpool=2)
+
+        self.block_2 = nn.Sequential(
+            self.conv_builder(50, 100, 1),
+            self.conv_builder(100, 150, 3, padding=1),
+            self.conv_builder(150, 300, 3, padding=1),
+            nn.MaxPool2d(2)
+        )
+
+        self.downsample_2 = self.downsample(1, 300, maxpool=4)
+
+        self.block_3 = nn.Sequential(
+            self.conv_builder(300, 500, 2, padding=1),
+            self.conv_builder(500, 750, 2),
+            nn.MaxPool2d(2)
+        )
+
+        self.downsample_3 = self.downsample(1, 750, maxpool=8)
+
+        self.lin_model = nn.Sequential(
+            nn.Linear(750*5*3, 1000),
+            nn.ReLU(),
+
+            nn.Linear(1000, 500),
+            nn.ReLU(),
+
+            nn.Linear(500, 200),
+            nn.ReLU(),
+
+            nn.Linear(200, out_class_num)
+        )
+
+    def forward(self, X):
+        out = self.block_1(X)
+        out += self.downsample_1(X)     #Skip connection
+        out = self.block_2(out)
+        out += self.downsample_2(X)     #Skip connection
+        out = self.block_3(out)
+        out += self.downsample_3(X)     #Skip connection
+        out = out.view(-1, 750*5*3)
+        out = self.lin_model(out)
+        return out
 
 
-# class cnn_model(nn.Module):
-#     def __init__(self, out_class_num):
-#         super(cnn_model, self).__init__()
-#
-#         self.block_1 = nn.Sequential(
-#             nn.Conv2d(1, 10, 3, stride=1),
-#             nn.ReLU(),
-#
-#             nn.Conv2d(10, 30, 3, stride=1),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2),
-#
-#             nn.Conv2d(30, 50, 3, stride=1),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2)
-#         )
-#
-#         self.block_2 = nn.Sequential(
-#             nn.Conv2d(50, 100, 2, stride=1),
-#             nn.ReLU(),
-#
-#             nn.Conv2d(100, 150, 2, stride=1),
-#             nn.ReLU()
-#         )
-#
-#         self.lin_model = nn.Sequential(
-#             nn.Linear(150 * 7 * 2, 500),
-#             nn.ReLU(),
-#
-#             nn.Linear(500, 200),
-#             nn.ReLU(),
-#
-#             nn.Linear(200, out_class_num)
-#         )
-#
-#     def forward(self, X):
-#         print(X.shape)
-#         out = self.block_1(X)
-#         print(out.shape)
-#         out = self.block_2(out)
-#         out = self.lin_model(out)
-#         return out
-#
+
+    def conv_builder(self, input_dim, output_dim, kernel, stride=1, padding=0):
+        conv = nn.Sequential(
+            nn.Conv2d(input_dim, output_dim, kernel, stride=stride, padding=padding),
+            nn.BatchNorm2d(output_dim),
+            nn.ReLU(),
+        )
+        return conv
+
+    def downsample(self, input_dim, output_dim, maxpool=0):
+        downsample = nn.Sequential(
+            nn.Conv2d(input_dim, output_dim, 1, stride=1),
+            nn.BatchNorm2d(output_dim),
+            nn.MaxPool2d(maxpool)
+        )
+        return downsample
+
